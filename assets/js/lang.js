b083f72607859d3d001420f7916fba0cc91dd4a0
Morpheus0x <morpheus0x@pm.me>
2022-08-27 14:53:45 +0200

    fixed correct language detection and switching, now supporting any path

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,5 +1,5 @@
 
-console.log('now minified and template ready')
+// console.log('now minified and template ready')
 const languages = [
 {{ range $.Site.Home.AllTranslations }}
   '{{ .Language }}',
@@ -9,7 +9,7 @@ const languages = [
 
 function toggleLang() {
   for (l of languages) {
-    console.log('l: ', l)
+    // console.log('l: ', l)
     if (l === document.documentElement.lang) {
       continue
     } else {
@@ -18,7 +18,7 @@ function toggleLang() {
   }
 }
 function switchLang(target, save) {
-  console.log('switchLang target: ', target, ' save: ', save)
+  // console.log('switchLang target: ', target, ' save: ', save)
   if (target === document.documentElement.lang) {
     return
   }
@@ -27,24 +27,36 @@ function switchLang(target, save) {
       if (save) {
         window.localStorage.setItem("lang", l)
       }
-      // console.log('replace')
-      if (l === 'en') {
-        window.location.replace("/")
+      let paths = document.location.pathname.split('/')
+      // console.log('paths: ', paths)
+      let offset = 0
+      if (languages.includes(paths[1])) {
+        offset = 2
       } else {
-        window.location.replace("/" + l) // TODO: find a way to also reliably do this for sub pages, maybe look at how its originally done
+        offset = 1
       }
+      // console.log(offset)
+      let newPath = ''
+      if (l !== 'en') {
+        newPath = "/" + l
+      }
+      for (p of paths.slice(offset)) {
+        newPath += '/' + p
+      }
+      // console.log('switching to path: ', newPath)
+      window.location.replace(newPath)
     }
   }
 }
 function manageLang() {
-  console.log(languages)
+  // console.log(languages)
   // console.log('language: ', {{ .Language }})
   const userLang = navigator.language || navigator.userLanguage; 
   // console.log('userLang: ', userLang)
   const lang = userLang.split('-')[0]
   let chosenLang = window.localStorage && window.localStorage.getItem("lang");
   if (lang === document.documentElement.lang) {
-    console.log(lang, document.documentElement.lang)
+    // console.log(lang, document.documentElement.lang)
     return
   }
   if (chosenLang == null) {
@@ -59,7 +71,7 @@ manageLang()
 
 
 function forceLang(e) {
-  console.log('switchLang target: ', target)
+  // console.log('switchLang target: ', target)
   if (target === document.documentElement.lang) {
     return
   }
