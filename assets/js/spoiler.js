ed22f8859575c5052de915eef64b1d8909cd22f0
Morpheus0x <morpheus0x@pm.me>
2022-12-19 09:52:36 +0100

    added spoiler shortcode, currently not working as intended

---
+++ assets/js/spoiler.js
@@ -0,0 +1,6 @@
+
+function toggleSpoiler() {
+    document.querySelector(".contentSelector").parentElement().children.toggle()
+}
+document.querySelector(".spoiler_block_show").addEventListener("click", toggleSpoiler)
+document.querySelector(".spoiler_block_hide").addEventListener("click", toggleSpoiler)
